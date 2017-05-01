6b3af20c75a5e0714d90ebfa9d25ab2d656b80de
ilya <ilyaaa.1997.13@gmail.com>
2017-05-01 17:32:05 +0000

    main menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+    public VBox menu;
+    public Button daltonBtn;
+    public Button colorBtn;
+    public Button trainingBtn;
+    public Button exitBtn;
+
+    public void startDaltonTest(){
+
+    }
+
+    public void startColorTest(){
+
+    }
+
+    public void startTraining(){
+
+    }
+
+    public void exit(){
+        System.exit(0);
+    }
 }
